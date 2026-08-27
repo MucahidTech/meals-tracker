6ea8d242c1b2814d13f00030b0772213c04d2abc
MucahidTech <mucahid.tech@gmail.com>
2026-08-27 09:29:26 +0300

    feat(app): initialize home screen with device info

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import HomeScreen from "./screens/home";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <HomeScreen />
     </View>
   );
 }
---
+++ src/app/screens/home/index.tsx
@@ -0,0 +1,14 @@
+import * as Device from "expo-device";
+import { Platform, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to Meals-Tracker App!</Text>
+      <Text>Running on: {Platform.OS}</Text>
+      <Text>Device Model: {Device.modelName}</Text>
+      <Text>Device Brand: {Device.brand}</Text>
+      <Text>OS Version: {Device.osVersion}</Text>
+    </View>
+  );
+}
