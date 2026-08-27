b3aabf91e85fa828f3b0148f1e3277008ced4ff2
MucahidTech <mucahid.tech@gmail.com>
2026-08-27 09:42:34 +0300

    feat(styles): establish global design system

--- src/app/screens/home/index.tsx
+++ src/app/screens/home/index.tsx
@@ -1,14 +1,16 @@
 import * as Device from "expo-device";
-import { Platform, Text, View } from "react-native";
+import { Platform, Text, StyleSheet, ScrollView } from "react-native";
+import { globalStyles } from "@/styles/global";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Welcome to Meals-Tracker App!</Text>
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>Welcome to Meals-Tracker App!</Text>
+      <Text style={globalStyles.sectionTitle}>Device Information</Text>
       <Text>Running on: {Platform.OS}</Text>
       <Text>Device Model: {Device.modelName}</Text>
       <Text>Device Brand: {Device.brand}</Text>
       <Text>OS Version: {Device.osVersion}</Text>
-    </View>
+    </ScrollView>
   );
 }
